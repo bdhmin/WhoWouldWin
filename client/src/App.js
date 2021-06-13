cbe5e03e33678c198ca537a22e29b5ecb6afc7cc
Bryan Min <43192371+bdmin00@users.noreply.github.com>
2021-06-13 14:26:34 -0700

    Created frontend api fetch from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+    };
+  }
+  componentDidMount() {
+    axios.get('/api/summoner')
+      .then((response => this.setState({users: response.data})))
+      .catch(function(error) {
+        console.log('Can\'t fetch:', error.message);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Users</h2>
+        <ul>
+        {this.state.users.map(user => 
+          <li key={user.id}>{user.name}, Level {user.summonerLevel}</li>
+        )}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -6,13 +6,15 @@ const port = 4000
 
 app.get('/api/summoner', cors(), (req, res) => {
 
-    // This is temp data if you don't have API Key
-    // const users = [
-    //     {id: 1, name: 'Bryan', gamertag: 'Piece of Paper'},
-    // ];
-    // res.json(users);
+  // This is temp data if you don't have API Key
+  // const users = [
+  //     {id: 1, name: 'Bryan', gamertag: 'Piece of Paper'},
+  // ];
+  // res.json(users);
 
-  axios.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Piece%20of%20Paper?api_key=RGAPI-d3632f7d-cc43-46bf-bc59-40335e140449')
+  // Piece%20of%20Paper
+  // nate025
+  axios.get('https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/s3b0?api_key=RGAPI-d3632f7d-cc43-46bf-bc59-40335e140449')
     .then((dataPiece) => {
       res.json([dataPiece.data]);
     })
